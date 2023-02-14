57b0f4859ff2ff2552146de8a6692204bd88635f
Raven <lmcuong537@gmail.com>
2023-02-14 13:31:35 +0700

    101% complete LinuxProcess

--- LinuxProcess/LinuxProcess.cpp
+++ LinuxProcess/LinuxProcess.cpp
@@ -4,47 +4,49 @@
 #include <string.h>
 #include <ctype.h>
 
-int main() 
-{
-    printf("PID\tPPID\tCMD\n");
+int main() {
+  printf("PID\tPPID\tCMD\n");
 
-    DIR *dir = opendir("/proc");
-    if (!dir) 
-    {
-        perror("opendir");
-        exit(EXIT_FAILURE);
+  DIR* proc_dir = opendir("/proc");
+  if (!proc_dir) {
+    printf("Error occured.");
+    return 1;
+  }
+
+  char status_path[128], cmdline_path[128], cmd[256], line[256];
+  while (struct dirent* dir_entry = readdir(proc_dir)) {
+    const char* pid_str = dir_entry->d_name;
+    if (!isdigit(*pid_str)) {
+      continue;
     }
 
-    char path[128], cmd[256];
-    while (struct dirent *ent = readdir(dir)) 
-    {
-        if (!isdigit(*ent->d_name))
-            continue;
-
-        snprintf(path, sizeof(path), "/proc/%s/status", ent->d_name);
-        FILE *file = fopen(path, "r");
-        if (!file)
-            continue;
-
-        int pid = atoi(ent->d_name), ppid = 0;
-        char line[256];
-        while (fgets(line, sizeof(line), file))
-            if (strncmp(line, "PPid:", 5) == 0)
-                ppid = atoi(line + 5);
-
-        fclose(file);
-
-        snprintf(path, sizeof(path), "/proc/%s/cmdline", ent->d_name);
-        file = fopen(path, "r");
-        if (!file)
-            continue;
-
-        fgets(cmd, sizeof(cmd), file);
-        fclose(file);
-
-        printf("%d\t%d\t%s\n", pid, ppid, cmd);
+    snprintf(status_path, sizeof(status_path), "/proc/%s/status", pid_str);
+    FILE* status_file = fopen(status_path, "r");
+    if (!status_file) {
+      continue;
     }
 
-    closedir(dir);
-    return EXIT_SUCCESS;
+    int pid = atoi(pid_str), ppid = 0;
+    while (fgets(line, sizeof(line), status_file)) {
+      if (strncmp(line, "PPid:", 5) == 0) {
+        ppid = atoi(line + 5);
+      }
+    }
+
+    fclose(status_file);
+
+    snprintf(cmdline_path, sizeof(cmdline_path), "/proc/%s/cmdline", pid_str);
+    FILE* cmdline_file = fopen(cmdline_path, "r");
+    if (!cmdline_file) {
+      continue;
+    }
+
+    fgets(cmd, sizeof(cmd), cmdline_file);
+    fclose(cmdline_file);
+
+    printf("%d\t%d\t%s\n", pid, ppid, cmd);
+  }
+
+  closedir(proc_dir);
+  return 0;
 }
